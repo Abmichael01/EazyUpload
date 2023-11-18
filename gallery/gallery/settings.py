977acb022f14c73b6dec2e53992ec29dbf554e2e
urkel <121265048+Abmichael01@users.noreply.github.com>
2023-11-18 12:06:41 +0100

    Update settings.py

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -138,4 +138,4 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
